6f1119a104bab88e965c3be6e8f887e2003f6a37
wolfmib <wolfmib@gmail.com>
2025-08-22 20:04:39 +0200

    finish the course4's handon next, do course5

---
+++ chagtp_suggest_install.sh
@@ -0,0 +1,17 @@
+pip install --upgrade pip setuptools wheel
+
+# Core scientific stack first
+pip install numpy pandas
+
+# Visualization
+pip install matplotlib
+
+# Machine Learning
+pip install scikit-learn
+
+# Deep Learning (PyTorch CPU version)
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+# Jupyter Notebook
+pip install notebook
+
